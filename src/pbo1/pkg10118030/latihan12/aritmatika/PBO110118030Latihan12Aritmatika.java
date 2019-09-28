c14e61d75677c69c084d830d22d9c1346110be36
arisulistio37 <arisulistio37@gmail.com>
2019-09-28 17:30:09 +0700

    Initial commit

---
+++ src/pbo1/pkg10118030/latihan12/aritmatika/PBO110118030Latihan12Aritmatika.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118030.latihan12.aritmatika;
+
+/**
+ *
+ * @author Ari Sulistio
+ */
+public class PBO110118030Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a x b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+        
+    }
+    
+}
